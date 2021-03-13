650d93c12a7c6b4aaee5009e963496cf20da887c
Thrisha <thrishavem30@gmail.com>
2021-03-13 22:40:42 +0530

    chk

--- SpringBootMongoDB/src/main/java/rc/SpringbootMongodbDemoApplication.java
+++ SpringBootMongoDB/src/main/java/rc/SpringbootMongodbDemoApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootMongodbDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMongodbDemoApplication.class, args);
+		//SpringApplication.run(SpringbootMongodbDemoApplication.class, args);
 	}
 }
